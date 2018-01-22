687d822c13a1e12068bf642df1a9f33da94b5bc4
Dmitriy Zharov <zharov.1512@gmail.com>
2018-01-22 17:43:59 +0300

    Initial Commit

---
+++ Legion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Legion
+//
+//  Created by Дмитрий Жаров on 22.01.2018.
+//  Copyright © 2018 SberTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
